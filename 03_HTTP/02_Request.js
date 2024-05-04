90687b260adb171ee553358dcff559b8aab12ff5
Shenhaoyi <shenhaoyi04@163.com>
2024-05-04 21:52:14 +0800

    获取路径和查询参数

--- 03_HTTP/02_Request.js
+++ 03_HTTP/02_Request.js
@@ -1,4 +1,5 @@
 import http from 'node:http';
+import url from 'node:url';
 
 const server = http.createServer((request, response) => {
   console.log(request.method); // 请求方法
@@ -10,6 +11,10 @@ const server = http.createServer((request, response) => {
       key 都进行小写处理
   */
   console.log(request.headers);
+
+  // 获取路径和查询参数
+  const { pathname: path1, query } = url.parse(request.url, true /* true 时，会将查询字符串解析为对象，即 query*/);
+  const { pathname: path2, searchParams } = new URL(request.url, `http://${request.headers.host}`);
 
   response.end('http');
 });
