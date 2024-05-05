91b2ed82bb2acf2b808f699b8e5b8e13f1249c58
Shenhaoyi <shenhaoyi04@163.com>
2024-05-05 21:26:14 +0800

    设置 mime 类型

--- 03_HTTP/05_static_server.js
+++ 03_HTTP/05_static_server.js
@@ -1,16 +1,31 @@
 import http from 'node:http';
 import fs from 'node:fs';
 import url from 'node:url';
+import path from 'node:path';
+
+const mimeMap = {
+  html: 'text/html',
+  css: 'text/css',
+  js: 'text/javascript',
+  png: 'image/png',
+  jpg: 'image/jpeg',
+  gif: 'image/gif',
+  mp4: 'video/mp4',
+  mp3: 'audio/mpeg',
+  json: 'application/json',
+};
 
 const server = http.createServer((request, response) => {
   const { pathname } = url.parse(request.url);
-  const path = process.cwd() + '/public' + (pathname === '/' ? '/index.html' : pathname);
-  fs.readFile(path, 'utf8', (error, data) => {
+  const filePath = process.cwd() + '/public' + (pathname === '/' ? '/index.html' : pathname);
+  const ext = path.extname(filePath).substring(1);
+  fs.readFile(filePath, 'utf8', (error, data) => {
     if (error) {
       response.writeHead(404);
       response.end('Not Found');
       return;
     }
+    response.setHeader('Content-Type', mimeMap[ext] || 'text/plain');
     response.end(data);
   });
 });
